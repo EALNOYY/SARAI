22fc217102e3f4a9e9262d63037eee5fca9afcbf
andrew <69625852+Andrew12347@users.noreply.github.com>
2022-08-01 17:59:24 -0500

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
-	month: 8, // required
-	day: 1, // required
+	month: 10, // required
+	day: 2, // required
 	hours: 1, // Default is 0 [0-23] integer
 	minutes: 1, // Default is 0 [0-59] integer
 	seconds: 1, // Default is 0 [0-59] integer
